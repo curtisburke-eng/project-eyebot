f7160cf82032157a873d2990491d77b66f30fd57
curtisburke-eng <curtisburke.eng@gmail.com>
2024-05-09 14:11:53 -0600

    Create a basic python node

--- src/eyebot_radio/setup.py
+++ src/eyebot_radio/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "eyebot_radio_node = src.eyebot_radio:main"
         ],
     },
 )
---
+++ src/eyebot_radio/src/eyebot_radio.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class eyebotRadioNode(Node):
+    # Define a consructor
+    def __init__(self):
+        super().__init__("eyebot_radio_node")
+        self.get_logger().info("Hello from ROS2")
+
+# Main function
+def main():
+
+    rclpy.init()
+    node = eyebotRadioNode()
+
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
