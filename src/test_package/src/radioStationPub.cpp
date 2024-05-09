eeb585fda066985cd0fc035bdf60c60c090b67b4
curtisburke-eng <curtisburke.eng@gmail.com>
2024-05-09 14:27:52 -0600

    Comment radioStation Test node for later debug

--- src/test_package/src/radioStationPub.cpp
+++ src/test_package/src/radioStationPub.cpp
@@ -11,39 +11,39 @@ using namespace std::chrono_literals;
 // TODO: Create a custom msg type eyebot_radio with members of enable and stationNum and volume? 
 
 
-class radioStationPub : public rclcpp::Node {
+// class radioStationPub : public rclcpp::Node {
 
-    public:
-        radioStationPub() : Node("radioStationPub"), count_(0) {
-            publisher_ = this->create_publisher<std_msgs::msg::Int8>("eyebot_radioStation", 10);
-            timer_ = this->create_wall_timer(500ms, std::bind(&radioStationPub::callback, this));
-        }
+//     public:
+//         radioStationPub() : Node("radioStationPub"), count_(0) {
+//             publisher_ = this->create_publisher<std_msgs::msg::Int8>("eyebot_radioStation", 10);
+//             timer_ = this->create_wall_timer(500ms, std::bind(&radioStationPub::callback, this));
+//         }
 
-    private:
-        void callback() {
-            auto message = std_msgs::msg::Int8();
+//     private:
+//         void callback() {
+//             auto message = std_msgs::msg::Int8();
             
-            if(count_ % 10 == 0) {
-                message.data = 2;                                                                               // THIS is the radio station number 
-            } else {
-                message.data = 1;                                                                               // THIS is the radio station number 
-            }
-            count_++;
+//             if(count_ % 10 == 0) {
+//                 message.data = 2;                                                                               // THIS is the radio station number 
+//             } else {
+//                 message.data = 1;                                                                               // THIS is the radio station number 
+//             }
+//             count_++;
 
-            RCLCPP_INFO(this->get_logger(), "Publishing Radio Station: %s", message.data.c_str());
-            publisher_->publish(message);
-        }
+//             RCLCPP_INFO(this->get_logger(), "Publishing Radio Station: %s", message.data.c_str());
+//             publisher_->publish(message);
+//         }
 
-        rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_;
-        size_t count_;
-};
+//         rclcpp::TimerBase::SharedPtr timer_;
+//         rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_;
+//         size_t count_;
+// };
 
 int main(int argc, char * argv[]) {
   
   rclcpp::init(argc, argv);
   
-  rclcpp::spin(std::make_shared<radioStationPub>());
+//   rclcpp::spin(std::make_shared<radioStationPub>());
   rclcpp::shutdown();
   
   return 0;
