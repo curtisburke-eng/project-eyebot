89ed963011194e9cd8ed022a06e9328b1cb02b3b
Scox-96 <scox0315@icloud.com>
2024-05-23 09:57:07 -0600

    clean up code

--- src/eyebot_controller/lcd/src/main.cpp
+++ src/eyebot_controller/lcd/src/main.cpp
@@ -7,16 +7,15 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+
+lcd.begin(16, 2);
+lcd.print("Diamond City");
+
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+
+lcd.setCursor(0, 1);
+lcd.print(millis() / 10);
+
 }
