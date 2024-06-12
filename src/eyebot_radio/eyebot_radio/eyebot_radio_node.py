5bdc5e4fe9e5fdc0df3770dc529b3333716dac68
curtisburke-eng <curtisburke.eng@gmail.com>
2024-06-11 23:13:51 -0600

    Use example script with pyaudio lib

--- src/eyebot_radio/eyebot_radio/eyebot_radio_node.py
+++ src/eyebot_radio/eyebot_radio/eyebot_radio_node.py
@@ -1,6 +1,10 @@
 #!/usr/bin python3
 import rclpy
 from rclpy.node import Node
+import pyaudio
+import wave
+import time
+
 
 class eyebotRadioNode(Node):
     # Define a consructor
@@ -11,11 +15,42 @@ class eyebotRadioNode(Node):
 # Main function
 def main():
 
-    rclpy.init()
-    node = eyebotRadioNode()
+    # rclpy.init()
+    # node = eyebotRadioNode()
 
-    rclpy.spin(node)
-    rclpy.shutdown()
+    # rclpy.spin(node)
+    # rclpy.shutdown()
+
+
+
+    paused = False    # global to track if the audio is paused
+
+
+    # you audio here
+    wf = wave.open('../../../audio/GNRhello.wav', 'rb')
+
+    # instantiate PyAudio
+    p = pyaudio.PyAudio()
+
+    # define callback
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
+
+    # open stream using callback
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()), channels=wf.getnchannels(), rate=wf.getframerate(), output=True, stream_callback=callback)
+
+    # start the stream
+    stream.start_stream()
+
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+    wf.close()
+
+    # close PyAudio
+    p.terminate()
+
 
 if __name__ == "__main__":
     try:
